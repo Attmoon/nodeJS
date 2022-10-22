9cc1c9dc6a8be0fab3e78caf5db8b34eb020528a
Attmoon <99157555+Attmoon@users.noreply.github.com>
2022-10-22 22:19:33 +0900

    first nodejs

---
+++ test.js
@@ -0,0 +1,6 @@
+const moment = require("moment");
+
+console.log("today", moment().format("YYYY-MM-DD"));
+console.log("today", moment().add(1, "day").format("YYYY-MM-DD"));
+console.log("today", moment().add(1, "week").format("YYYY-MM-DD"));
+console.log("today", moment().add(1, "month").format("YYYY-MM-DD"));
